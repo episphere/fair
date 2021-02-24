90f0e8399db4d437a9a5eb49eb7eccdb198fb700
Jonas Almeida <jalmeida@mathbiol.org>
2021-02-24 17:57:23 -0500

    lights working

--- fair.js
+++ fair.js
@@ -1,1 +1,97 @@
 console.log('fair.js loaded')
+
+fair={date:Date(),parms:{},checklist:[]}
+
+fair.getparms=q=>{
+    location.search.slice(1).split('&').forEach(aa=>{
+        aa = aa.split('=')
+        fair.parms[aa[0]]=decodeURIComponent(aa[1])
+    })
+    return fair.parms
+}
+
+fair.getparms()
+
+fair.ui=async(
+        el=document.getElementById('FAIRreport'),
+        url=fair.parms.url
+    )=>{
+    console.log(`FAIR report for ${url}`) // https://data.cdc.gov/resource/muzy-jte6.json
+    let h= '<hr><div style="background-color:silver;width:100%">'
+    h += `URL: <input size="50" id="urlInput"> <button id="startFAIR">Start</button>`
+    h += '</div><hr>'
+    //h += `<h3>Dereferenceability</h3>`
+    h += '<div><ol id="FAIRchecklist"></ol></div>'
+    el.innerHTML=h
+    urlInput.value=fair.parms.url||""
+    if(urlInput.value.length>1){
+        fair.parms.url=urlInput.value
+        fair.deref(url=fair.parms.url,cont=true)
+    }
+    startFAIR.onclick=()=>{
+        FAIRchecklist.innerHTML=''
+        fair.parms.url=urlInput.value
+        fair.deref(url=fair.parms.url,cont=true)
+    }
+
+    return el
+}
+
+fair.deref = async(url=fair.parms.url,cont)=>{
+    fetch(url)
+      .catch((error)=>{
+          let li = document.createElement('li')
+          FAIRchecklist.appendChild(li)
+          li.innerHTML=`${error}`
+          li.style.color="darkred"
+          trafficLight.src="red.png"
+      })
+      .then(r1 => {
+        r1.json()
+            .catch((error)=>{
+                let li = document.createElement('li')
+                FAIRchecklist.appendChild(li)
+                li.innerHTML=`${error}`
+                li.style.color="orange"
+                setTimeout((_=>{trafficLight.src="yellow.png"}),1000)
+            })
+            .then(data => {
+                let li = document.createElement('li')
+                FAIRchecklist.appendChild(li)
+                li.innerHTML=`<p>type: ${r1.type}</p>`
+                li.style.color="darkgreen"
+                trafficLight.src="green.png"
+                let ta = document.createElement('textArea')
+                li.appendChild(ta)
+                ta.style.backgroundColor='black'
+                ta.style.color='lime'
+                ta.innerText=JSON.stringify(data,null,3)
+                ta.style.width='100%'
+                ta.style.height="50em"
+            })
+      })//.catch(err=>console.log('error:',err));
+    
+    /*
+    try {
+        let res = await (await fetch(url))
+        let h = `type: ${res.type}`
+        li.innerHTML=h
+        li.style.color="darkgreen"
+        console.log(JSON.stringify(res,null,3))
+    }
+    catch(err){
+
+    }
+    */
+    //return li
+}
+
+
+if(document.getElementById('FAIRreport')){
+    //if
+    fair.ui()
+}
+
+if(typeof(define)!='undefined'){
+    define(fair)
+}
