91427d60868ef75bcaf74888a0b2a8589ac56c80
Jonas Almeida <jalmeida@mathbiol.org>
2021-02-24 20:56:03 -0500

    resize

--- fair.js
+++ fair.js
@@ -21,7 +21,7 @@ fair.ui=async(
     h += `URL: <input size="50" id="urlInput"> <button id="startFAIR">Start</button>`
     h += '</div><hr>'
     //h += `<h3>Dereferenceability</h3>`
-    h += '<div><ol id="FAIRchecklist"></ol></div>'
+    h += '<div style="width:100%"><ol id="FAIRchecklist" style="width:100%"></ol></div>'
     el.innerHTML=h
     urlInput.value=fair.parms.url||""
     if(urlInput.value.length>1){
@@ -65,9 +65,9 @@ fair.deref = async(url=fair.parms.url,cont)=>{
                 li.appendChild(ta)
                 ta.style.backgroundColor='black'
                 ta.style.color='lime'
-                ta.innerText=JSON.stringify(data,null,3)
-                ta.style.width='100%'
-                ta.style.height="50em"
+                ta.value=JSON.stringify(data,null,3)
+                ta.style.width="60em"
+                ta.style.height="30em"
             })
       })//.catch(err=>console.log('error:',err));
     
