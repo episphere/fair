da83564aff45ca1e9f1580dd53786c673ad3cad4
Jonas Almeida <jalmeida@mathbiol.org>
2021-02-25 23:48:59 -0500

    open window

--- fair.js
+++ fair.js
@@ -18,7 +18,7 @@ fair.ui=async(
     )=>{
     console.log(`FAIR report for ${url}`) // https://data.cdc.gov/resource/muzy-jte6.json
     let h= '<hr><div style="background-color:silver;width:100%">'
-    h += `URL: <input size="75" id="urlInput"> <button id="startFAIR">Start</button>`
+    h += `URL: <input size="54" id="urlInput" style="color:navy"> <button id="startFAIR">Start</button> <button id="openURL">Open</button>`
     h += '</div><hr>'
     //h += `<h3>Dereferenceability</h3>`
     h += '<div style="width:100%"><ol id="FAIRchecklist" style="width:100%"></ol></div>'
@@ -33,6 +33,9 @@ fair.ui=async(
         //FAIRchecklist.innerHTML=''
         //fair.parms.url=urlInput.value
         //fair.deref(url=fair.parms.url,cont=true)
+    }
+    openURL.onclick=_=>{
+        window.open(urlInput.value)
     }
 
     return el
@@ -65,9 +68,10 @@ fair.deref = async(url=fair.parms.url,cont)=>{
                 let ta = document.createElement('textArea')
                 li.appendChild(ta)
                 ta.style.backgroundColor='black'
+                ta.style.fontSize='small'
                 ta.style.color='lime'
                 ta.value=JSON.stringify(data,null,3)
-                ta.style.width="60em"
+                ta.style.width="50em"
                 ta.style.height="30em"
             })
       })//.catch(err=>console.log('error:',err));
